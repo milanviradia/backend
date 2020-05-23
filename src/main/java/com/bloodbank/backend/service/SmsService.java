49b166f337b99f8cb8f9bd3de490cf74a8f63dd1
mayursheliya51 <mayursheliya51@gmail.com>
2020-05-23 19:20:46 +0530

    pull changes

--- src/main/java/com/bloodbank/backend/service/SmsService.java
+++ src/main/java/com/bloodbank/backend/service/SmsService.java
@@ -4,19 +4,10 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
 @Service
-@ConfigurationProperties(prefix = "twillio.credentials")
-@Configuration("employeeProperties")
 public class SmsService {
-	@Value("${twillio.credentials.sid}")
-	private String ACCOUNT_SID;
-	@Value("${twillio.credentials.token}")
-	private String AUTH_TOKEN;
-	
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
 	public void sendSMS(String mobile_no,String address) {
 		Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
